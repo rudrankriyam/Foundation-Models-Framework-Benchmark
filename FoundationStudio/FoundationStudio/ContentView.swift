e9c6e3dfc5ba5959b54252dca8b9c2cf9cda0af0
Rudrank Riyam <rudrankriyam@gmail.com>
2025-10-25 15:04:30 +0530

    Initial setup of FoundationStudio project

---
+++ FoundationStudio/FoundationStudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationStudio
+//
+//  Created by Rudrank Riyam on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationStudio/FoundationStudio/FoundationStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationStudioApp.swift
+//  FoundationStudio
+//
+//  Created by Rudrank Riyam on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
